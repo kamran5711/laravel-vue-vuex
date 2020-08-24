5a56f8852597eabc2dcf90bad92fa6865cd8baa8
kamran5711 <muhammad.kamran.5711@gmail.com>
2020-08-23 21:52:19 -0700

    laravel-vue-vuex-auth

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php 
+
+Route::group(['prefix'=> 'auth'], function(){
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
